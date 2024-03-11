151b7fca388f60029fc8c55b95490e9ca64e2551
Alexandr Older <alex.older77@gmail.com>
2024-03-11 13:29:51 +0300

    какие-то изменения

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 "use strict";
 
 /* Задание на урок:
@@ -44,3 +45,75 @@ const a = prompt("Один из последних просмотренных ф
 
 console.log(numberOfFilms);
 console.log(personalMovieDB);
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+// 	console.log("Все довольны!");
+// } else {
+// 	console.log("Мы уходим");
+// }
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+
+console.log( NaN || 2 || undefined );
+
+
+console.log( NaN && 2 && undefined );
+
+
+console.log( 1 && 2 && 3 );
+
+
+console.log( !1 && 2 || !3 );
+
+
+console.log( 25 || null && !3 );
+
+
+console.log( NaN || null || !3 || undefined || 5);
+
+
+console.log( NaN || null && !3 && undefined || 5);
+
+
+console.log( 5 === 5 && 3 > 1 || 5);
+
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+// 	console.log("Done!");
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// console.log(hamburger);
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+// 	console.log("Done!");
+// }
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+	console.log("Done!");
+}
