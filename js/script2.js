25d208e8a8a44120fdf9eb755d2e9bd88ab06211
Alexandr Older <alex.older77@gmail.com>
2024-03-16 18:58:24 +0300

    another commit

--- js/script2.js
+++ js/script2.js
@@ -95,3 +95,122 @@
 
 // console.log(arrayOfNumbers);
 // return arrayOfNumbers;
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert(amount, curr) {
+// 	console.log(curr * amount);
+// }
+
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+// 	console.log(curr * amount);
+// 	return curr * amount;
+// }
+
+// function promotion(result) {
+// 	console.log(result * discount);
+// }
+
+// promotion(convert(500, usdCurr));
+
+// function test() {
+// 	for(let i = 0; i < 5; i++) {
+// 		console.log(i);
+// 		if (i === 2) return;
+// 	}
+// 	console.log("Done");
+// }
+// test();
+
+// function doNothing() {}
+// console.log(doNothing());
+
+// 1) Создайте функцию, которая будет на вход принимать 1 аргумент с именем человека и возвращать строку. 
+
+// Пример: вызов функции sayHello('Антон') возвращает строку "Привет, Антон!". В решении вызывать функцию не нужно, программа сделает это за вас.
+
+// P.S. возвращать - это использовать ключевое слово return. 
+
+// P.S.S. Это классическая функция-модификатор, которых мы дальше по курсу будем создавать еще много в разных видах.
+
+// function sayHello(name) {
+// 	return "Привет, " + name + "!";
+// }
+// console.log(sayHello("Антон"));
+
+// 2) Создайте функцию, которая принимает в себя 1 аргумент в виде целого числа и возвращает массив из трех чисел: одно на 1 меньше, сам аргумент, и число на 1 больше.
+
+// Пример: вызов функции returnNeighboringNumbers(5) возвращает массив в виде [4, 5, 6].
+
+
+// function returnNeighboringNumbers (num) {
+// 	return [num - 1, num, num + 1]
+// }
+
+// // return NeighboringNumbers(5);
+// console.log(returnNeighboringNumbers(5));
+
+// 3) Создайте функцию, которая будет принимать в себя 2 аргумента, оба числа. Первое число - это база, второе число - это сколько раз нужно будет повторить это число в прогрессии. (Смотри пример ниже). Функция должна возвращать строку (или число в особых случаях, о которых ниже), где эти числа идут по порядку, разделенные тремя дефисами "---". После последнего числа их не должно быть. 
+
+// Если второй аргумент не является числом, равен или меньше нуля - то возвращать просто первый аргумент. (Проверяем через оператор typeof)
+
+// Примеры:
+
+// Вызов функции getMathResult(5, 3) даст ответ 5---10---15
+
+// Вызов функции getMathResult(3, 10) даст ответ 3---6---9---12---15---18---21---24---27---30
+
+// Вызов функции getMathResult(10, 5) даст ответ 10---20---30---40---50
+
+// Вызов функции getMathResult(10, '5') даст ответ 10
+
+// Вызов функции getMathResult(10, 0) даст ответ 10
+
+// Вызов функции getMathResult(20, -5) даст ответ 20
+
+// function getMathResult(a, b) {
+// 	for (let i = 1; i <= b; i++) {
+// 		a + a;
+// 	}
+// }
+// console.log(getMathResult(5, 3));
+
+
+
+// let a = "ops";
+// a += "--- ";
+
+// console.log(a);
+
+// function getMathResult(num, times) {
+// 	if (typeof(times) !== "number" || times <= 0) {
+// 		return num;
+// 	}
+
+// 	let str = "";
+
+// 	for (let i = 1; i <= times; i++) {
+// 		if (i === times) {
+// 			str += `${num * i}`;
+// 			// Тут без черточек в конце
+// 		} else {
+// 			str += `${num * i}---`;
+// 			// Это тоже самое, что и
+// 			// str = str + num * i + "---"
+// 		}
+// 	}
+
+// 	return str;
+// }
+
+// console.log(getMathResult(10, 5));
+
+const test = "128qkw3251654654fhoaisfhoiasufh";
+console.log(parseInt(test));
